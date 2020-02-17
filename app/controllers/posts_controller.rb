72fb4f72aee7ae1ed4b5ba3d1c3aa5be347d6f41
Hiroaki-Nakajima <h.19860820@gmail.com>
2020-02-17 15:40:28 +0900

    index作成
    This reverts commit 8bbd261df596add9e6054ee58dcc713bd547fea3.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,5 @@
 class PostsController < ApplicationController
   def index
-    @post = Post.new
-    @posts = Post.all
-    @search_params = user_search_params
-    @search_post = Post.search(@search_params).includes(:prefecture)
-  end
-
-  def create
-    Post.create(post_params)
-    redirect_to root_path
-  end
-
-  private
-  def post_params
-    params.require(:post).permit(:text)
-  end
-
-  def user_search_params
-    params.fetch(:search, {}).permit(:text)
+    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_035036) do
-
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
